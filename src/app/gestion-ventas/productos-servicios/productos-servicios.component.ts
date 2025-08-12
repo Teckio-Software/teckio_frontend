39b7bbb8cda77b4bbf9b71f0fc39e17b70da911b
xYairrrx <221615354+YairAG@users.noreply.github.com>
2025-08-12 11:18:56 -0600

    listas en cada input

--- src/app/gestion-ventas/productos-servicios/productos-servicios.component.ts
+++ src/app/gestion-ventas/productos-servicios/productos-servicios.component.ts
@@ -1,5 +1,4 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { ProductoYServicioConjunto } from '../productos/productos';
 import { ProductoYServicioService } from 'src/app/productos-y-servicios/productoyservicio.service';
 import { SeguridadService } from 'src/app/seguridad/seguridad.service';
@@ -19,9 +18,27 @@ import { SubcategoriaProductoServicio } from 'src/app/facturacion/subcategoria-p
   templateUrl: './productos-servicios.component.html',
 })
 export class ProductosServiciosComponent {
+  constructor(
+    private _productoYServicioService: ProductoYServicioService,
+    private _seguridadService: SeguridadService,
+    private _unidadService: UnidadService,
+    private _unidadSatService: UnidadSatService,
+    private _categoriaProdSerservice: CategoriaProductoServicioService,
+    private _subcategoriaProdSerservice: SubcategoriaProductoServicioService,
+    private _productoServicioSatService: ProductoServicioSatService
+  ) {
+    let IdEmpresa = _seguridadService.obtenIdEmpresaLocalStorage();
+    this.selectedEmpresa = Number(IdEmpresa);
+  }
 
+  @HostListener('document:click', ['$event'])
+  clickFuera(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.relative')) {
+      this.cerrarTodasListas();
+    }
+  }
   selectedEmpresa: number = 0;
-
   listaProductosYServicios: ProductoYServicioConjunto[] = [];
   listaProductosYServiciosSat: ProductoServicioSat[] = [];
   listaProductosYServiciosSatAux: ProductoServicioSat[] = [];
@@ -29,20 +46,23 @@ export class ProductosServiciosComponent {
   listaUnidadesSat: UnidadSatDTO[] = [];
   listaCategorias: CategoriaProductoServicioDTO[] = [];
   listaSubcategorias: SubcategoriaProductoServicio[] = [];
+  textoBusquedaUnidad = '';
+  textoBusquedaPyS = '';
+  textoBusquedaSAT = '';
+  textoCategoria = '';
+  textoSubCategoria = '';
+  unidadesFiltradas: UnidadDTO[] = [];
+  productoyServFiltrados: ProductoServicioSat[] = [];
+  unidadSATFiltradas: UnidadSatDTO[] = [];
+  categoriaFiltradas: CategoriaProductoServicioDTO[] = [];
+  subcategoriaFiltradas: SubcategoriaProductoServicio[] = [];
+  mostrarListaUnidades = false;
+  mostrarListaProductosYServicios = false;
+  mostrarListaUnidadesSat = false;
+  mostrarListaCategorias = false;
+  mostrarListaSubCategorias = false;
 
-  constructor(private _productoYServicioService: ProductoYServicioService,
-    private _seguridadService: SeguridadService,
-    private _unidadService: UnidadService,
-    private _unidadSatService: UnidadSatService,
-    private _categoriaProdSerservice: CategoriaProductoServicioService,
-    private _subcategoriaProdSerservice: SubcategoriaProductoServicioService,
-    private _productoServicioSatService: ProductoServicioSatService,
-  ){
-    let IdEmpresa = _seguridadService.obtenIdEmpresaLocalStorage();
-    this.selectedEmpresa = Number(IdEmpresa);
-  }
-
-  ngOnInit(){
+  ngOnInit() {
     this.CargarProductosYServicios();
     this.cargarUnidades();
     this.cargarUnidadesSat();
@@ -51,44 +71,163 @@ export class ProductosServiciosComponent {
     this.cargarSubcategoriasProdSer();
   }
 
-  cargarUnidades(){
-    this._unidadService.ObtenerTodos(this.selectedEmpresa).subscribe((resp)=>{
-      this.listaUnidades = resp;      
-    })
+  // unidad
+  cargarUnidades() {
+    this._unidadService.ObtenerTodos(this.selectedEmpresa).subscribe((resp) => {
+      this.listaUnidades = resp;
+    });
   }
 
-  cargarUnidadesSat(){
-    this._unidadSatService.ObtenerTodos(this.selectedEmpresa).subscribe((resp)=>{
-      this.listaUnidadesSat = resp;
-    })
+  abrirLista() {
+    this.unidadesFiltradas = [...this.listaUnidades];
+    this.cerrarTodasListas();
+    this.mostrarListaUnidades = true;
   }
 
-  cargarProdYSerSat(){
-    this._productoServicioSatService.ObtenerTodos(this.selectedEmpresa).subscribe((resp)=>{
-      this.listaProductosYServiciosSat = resp;
-      this.listaProductosYServiciosSatAux = this.listaProductosYServiciosSat.slice(0,100);
-      console.log(this.listaProductosYServiciosSatAux);
-      
-    })
+  filtrarUnidades(event: Event) {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.textoBusquedaUnidad = valor;
+    this.unidadesFiltradas = this.listaUnidades.filter((u) =>
+      u.descripcion.toLowerCase().includes(valor)
+    );
+    this.mostrarListaUnidades = true;
   }
 
-  cargarCategoriasProdSer(){
-    this._categoriaProdSerservice.ObtenerTodos(this.selectedEmpresa).subscribe((resp)=>{
-    this.listaCategorias = resp;      
-    })
+  seleccionarUnidad(unidad: UnidadDTO) {
+    this.textoBusquedaUnidad = unidad.descripcion;
+    this.unidadesFiltradas = [];
+    this.mostrarListaUnidades = false;
   }
 
-  cargarSubcategoriasProdSer(){
-    this._subcategoriaProdSerservice.ObtenerTodos(this.selectedEmpresa).subscribe((resp)=>{
-      this.listaSubcategorias = resp;
-    })
+  //producto y servicio
+  cargarProdYSerSat() {
+    this._productoServicioSatService
+      .ObtenerTodos(this.selectedEmpresa)
+      .subscribe((resp) => {
+        this.listaProductosYServiciosSat = resp;
+        //muestra 100 resultados
+        this.listaProductosYServiciosSatAux =
+          this.listaProductosYServiciosSat.slice(0, 100);
+      });
   }
 
-  CargarProductosYServicios(){
-    this._productoYServicioService.obtenerConjuntos(this.selectedEmpresa).subscribe((resp)=>{
-      this.listaProductosYServicios = resp;
-      console.log(this.listaProductosYServicios);
-    })
+  abrirListaProductoyServ() {
+    this.productoyServFiltrados = [...this.listaProductosYServiciosSatAux];
+    this.cerrarTodasListas();
+    this.mostrarListaProductosYServicios = true;
+  }
+
+  filtrarProductosYServicios(event: Event) {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.textoBusquedaPyS = valor;
+    this.productoyServFiltrados = this.listaProductosYServiciosSatAux.filter(
+      (p) => p.descripcion.toLowerCase().includes(valor)
+    );
+    this.mostrarListaProductosYServicios = true;
+  }
+
+  seleccionarProductoServicio(productoServicio: ProductoServicioSat) {
+    this.textoBusquedaPyS = productoServicio.descripcion;
+    this.productoyServFiltrados = [];
+    this.mostrarListaProductosYServicios = false;
+  }
+
+  //unidad sat
+  cargarUnidadesSat() {
+    this._unidadSatService
+      .ObtenerTodos(this.selectedEmpresa)
+      .subscribe((resp) => {
+        this.listaUnidadesSat = resp;
+      });
+  }
+
+  abrirListaUnidadesSat() {
+    this.unidadSATFiltradas = [...this.listaUnidadesSat];
+    this.cerrarTodasListas();
+    this.mostrarListaUnidadesSat = true;
+  }
+
+  filtrarUnidadesSat(event: Event) {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.textoBusquedaSAT = valor;
+    this.unidadSATFiltradas = this.listaUnidadesSat.filter((u) =>
+      u.nombre.toLowerCase().includes(valor)
+    );
+    this.mostrarListaUnidadesSat = true;
+  }
+
+  seleccionarUnidadSat(unidad: UnidadSatDTO) {
+    this.textoBusquedaSAT = unidad.nombre;
+    this.unidadSATFiltradas = [];
+    this.mostrarListaUnidadesSat = false;
+  }
+
+  //categoria
+  cargarCategoriasProdSer() {
+    this._categoriaProdSerservice
+      .ObtenerTodos(this.selectedEmpresa)
+      .subscribe((resp) => {
+        this.listaCategorias = resp;
+      });
+  }
+
+  abrirListaCategoria() {
+    this.categoriaFiltradas = [...this.listaCategorias];
+    this.cerrarTodasListas();
+    this.mostrarListaCategorias = true;
+  }
+
+  filtrarCategorias(event: Event) {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.textoCategoria = valor;
+    this.categoriaFiltradas = this.listaCategorias.filter((c) =>
+      c.descripcion.toLowerCase().includes(valor)
+    );
+    this.mostrarListaCategorias = true;
+  }
+
+  seleccionarCategoria(categoria: CategoriaProductoServicioDTO) {
+    this.textoCategoria = categoria.descripcion;
+    this.categoriaFiltradas = [];
+    this.mostrarListaCategorias = false;
+  }
+
+  //subcategoria
+  cargarSubcategoriasProdSer() {
+    this._subcategoriaProdSerservice
+      .ObtenerTodos(this.selectedEmpresa)
+      .subscribe((resp) => {
+        this.listaSubcategorias = resp;
+      });
+  }
+
+  abrirListaSubCategoria() {
+    this.subcategoriaFiltradas = [...this.listaSubcategorias];
+    this.cerrarTodasListas();
+    this.mostrarListaSubCategorias = true;
+  }
+
+  filtrarSubCategorias(event: Event) {
+    const valor = (event.target as HTMLInputElement).value.toLowerCase();
+    this.textoSubCategoria = valor;
+    this.subcategoriaFiltradas = this.listaSubcategorias.filter((c) =>
+      c.descripcion.toLowerCase().includes(valor)
+    );
+    this.mostrarListaSubCategorias = true;
+  }
+
+  seleccionarSubCategoria(subcategoria: SubcategoriaProductoServicio) {
+    this.textoSubCategoria = subcategoria.descripcion;
+    this.subcategoriaFiltradas = [];
+    this.mostrarListaSubCategorias = false;
+  }
+
+  CargarProductosYServicios() {
+    this._productoYServicioService
+      .obtenerConjuntos(this.selectedEmpresa)
+      .subscribe((resp) => {
+        this.listaProductosYServicios = resp;
+      });
   }
 
   isModalAddOpen = false;
@@ -105,6 +244,18 @@ export class ProductosServiciosComponent {
   cerrarModal() {
     this.isModalAddOpen = false;
     this.isModalInfoOpen = false;
+    this.textoBusquedaUnidad = '';
+    this.textoBusquedaPyS = '';
+    this.textoBusquedaSAT = '';
+    this.cerrarTodasListas();
+  }
+
+  cerrarTodasListas() {
+    this.mostrarListaUnidades = false;
+    this.mostrarListaProductosYServicios = false;
+    this.mostrarListaUnidadesSat = false;
+    this.mostrarListaCategorias = false;
+    this.mostrarListaSubCategorias = false;
   }
 
   detenerCierre(event: MouseEvent) {
--- src/app/gestion-ventas/productos-servicios/productos-servicios.module.ts
+++ src/app/gestion-ventas/productos-servicios/productos-servicios.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductosServiciosRoutingModule } from './productos-servicios.routing';
 import { ProductosServiciosComponent } from './productos-servicios.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [ProductosServiciosComponent],
-  imports: [CommonModule, ProductosServiciosRoutingModule],
+  imports: [CommonModule, ProductosServiciosRoutingModule, ScrollingModule],
 })
 export class ProductosServiciosModule {}
