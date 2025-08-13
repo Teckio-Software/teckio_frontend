34ead95de02538aa9c26b5b3a73cd49cf74deab4
RJavvil <jgama@teckio.mx>
2025-08-13 10:38:26 -0600

    Endpoints para crear y eliminar productos y servicios

--- src/app/facturacion/insumoxproductoyservicio/insumoxproductoyservicio.service.ts
+++ src/app/facturacion/insumoxproductoyservicio/insumoxproductoyservicio.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { InsumoXProductoYServicioDTO } from './ts.insumoxproductoyservicio';
+import { RespuestaDTO } from 'src/app/utilidades/tsUtilidades';
 
 @Injectable({providedIn: 'root'})
 export class InsumoXProductoYServicioService {
@@ -11,5 +12,13 @@ export class InsumoXProductoYServicioService {
 
       public obtenerPorProdyser(idEmpresa:number, idProdySer: number){
             return this.httpClient.get<InsumoXProductoYServicioDTO[]>(`${this.apiUrl}/${idEmpresa}/obtenerXIdProdYSer/${idProdySer}`);
+      }
+
+      public crear(idEmpresa:number, insumo: InsumoXProductoYServicioDTO){
+            return this.httpClient.post<RespuestaDTO>(`${this.apiUrl}/${idEmpresa}/crear`,insumo);
+      }
+
+      public eliminar(idEmpresa:number, insumo: number){
+            return this.httpClient.delete<RespuestaDTO>(`${this.apiUrl}/${idEmpresa}/eliminar/`+insumo);
       }
 }
