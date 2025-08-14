e06dc93cd706fab94df3cf576de1c222245a409b
xYairrrx <221615354+YairAG@users.noreply.github.com>
2025-08-14 13:53:22 -0600

    checkbox de almacenes

--- src/app/inventario/almacen/almacen/almacen.component.ts
+++ src/app/inventario/almacen/almacen/almacen.component.ts
@@ -1,4 +1,13 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { AlmacenService } from '../almacen.service';
 import { MatTable } from '@angular/material/table';
 import { almacenCreacionDTO, almacenDTO } from '../almacen';
@@ -14,188 +23,217 @@ import Swal from 'sweetalert2';
 import { EmpresaService } from 'src/app/catalogos/empresas/empresa.service';
 
 @Component({
-    selector: 'app-almacen',
-    templateUrl: './almacen.component.html',
-    styleUrls: ['./almacen.component.css']
+  selector: 'app-almacen',
+  templateUrl: './almacen.component.html',
+  styleUrls: ['./almacen.component.css'],
 })
-
 export class AlmacenComponent implements OnInit {
-    @ViewChild('NuevoAlmacen', { static: true })
-    dialogNuevoAlmacen!: TemplateRef<any>;
-    proyectos!: proyectoDTO[];
-    proyectoSeleccionado !: boolean;
-    idProyecto: number = 0;
-    form!: FormGroup;
-    ideditaAlmacen: number = 0;
-    selectedEmpresa: number = 0;
-    mostrarCentrales: boolean = false;
+  @ViewChild('NuevoAlmacen', { static: true })
+  dialogNuevoAlmacen!: TemplateRef<any>;
+  proyectos!: proyectoDTO[];
+  proyectoSeleccionado!: boolean;
+  idProyecto: number = 0;
+  form!: FormGroup;
+  ideditaAlmacen: number = 0;
+  selectedEmpresa: number = 0;
+  mostrarCentrales: boolean = false;
 
-    panelActivado: boolean = false;
-    constructor(private almacenService: AlmacenService
-        , private empresaService: EmpresaService
-        , private _SeguridadEmpresa: SeguridadService
-        , private _snackBar: MatSnackBar
-        , private formBuilder: FormBuilder
-        , private proyectoService: ProyectoService
-        , private dialog: MatDialog
-    ) {
-        let idEmpresa = _SeguridadEmpresa.obtenIdEmpresaLocalStorage();
-        let idProyecto = _SeguridadEmpresa.obtenerIdProyectoLocalStorage();
-        this.idProyecto = Number(idProyecto);
+  panelActivado: boolean = false;
+  constructor(
+    private almacenService: AlmacenService,
+    private empresaService: EmpresaService,
+    private _SeguridadEmpresa: SeguridadService,
+    private _snackBar: MatSnackBar,
+    private formBuilder: FormBuilder,
+    private proyectoService: ProyectoService,
+    private dialog: MatDialog
+  ) {
+    let idEmpresa = _SeguridadEmpresa.obtenIdEmpresaLocalStorage();
+    let idProyecto = _SeguridadEmpresa.obtenerIdProyectoLocalStorage();
+    this.idProyecto = Number(idProyecto);
     this.selectedEmpresa = Number(idEmpresa);
-    }
+  }
 
-    @ViewChild('table')
-    table?: MatTable<any>;
-    almacenes!: almacenDTO[];
-    nuevoalmacen : almacenDTO = {
-        id: 0,
-        codigo: '',
-        almacenNombre: '',
-        central: false,
-        responsable: '',
-        domicilio: '',
-        colonia: '',
-        ciudad: '',
-        telefono: '',
-        idProyecto: 0
-    }
-    almacen: almacenCreacionDTO = {
-        codigo: '',
-        almacenNombre: '',
-        central: false,
-        responsable: '',
-        domicilio: '',
-        colonia: '',
-        ciudad: '',
-        telefono: '',
-        idProyecto: 0
-    };
+  @ViewChild('table')
+  table?: MatTable<any>;
+  almacenes!: almacenDTO[];
+  nuevoalmacen: almacenDTO = {
+    id: 0,
+    codigo: '',
+    almacenNombre: '',
+    central: false,
+    responsable: '',
+    domicilio: '',
+    colonia: '',
+    ciudad: '',
+    telefono: '',
+    idProyecto: 0,
+  };
+  almacen: almacenCreacionDTO = {
+    codigo: '',
+    almacenNombre: '',
+    central: false,
+    responsable: '',
+    domicilio: '',
+    colonia: '',
+    ciudad: '',
+    telefono: '',
+    idProyecto: 0,
+  };
 
-    ngOnInit(): void {
-        this.traerInformacion();
-        this.proyectoService.obtener(this.selectedEmpresa).subscribe((datos) => {
-            this.proyectos = datos;
-        })
-        this.form = this.formBuilder.group({
-            id: [0, { validators: [], },]
-            , codigo: ['', { validators: [], },]
-            , almacenNombre: ['', { validators: [], },]
-            , central: ['', { validators: [], },]
-            , responsable: ['', { validators: [], },]
-            , domicilio: ['', { validators: [], },]
-            , colonia: ['', { validators: [], },]
-            , ciudad: ['', { validators: [], },]
-            , telefono: ['', { validators: [], },]
-            , idProyecto: [0, { validators: [], },]
-        });
-    }
+  ngOnInit(): void {
+    this.traerInformacion();
+    this.proyectoService.obtener(this.selectedEmpresa).subscribe((datos) => {
+      this.proyectos = datos;
+    });
+    this.form = this.formBuilder.group({
+      id: [0, { validators: [] }],
+      codigo: ['', { validators: [] }],
+      almacenNombre: ['', { validators: [] }],
+      central: ['', { validators: [] }],
+      responsable: ['', { validators: [] }],
+      domicilio: ['', { validators: [] }],
+      colonia: ['', { validators: [] }],
+      ciudad: ['', { validators: [] }],
+      telefono: ['', { validators: [] }],
+      idProyecto: [0, { validators: [] }],
+    });
+  }
 
-    traerInformacion(
+  traerInformacion() {
+    this.proyectoSeleccionado = true;
+
+    this.almacenService
+      .obtenerXIdProyecto(this.idProyecto, this.selectedEmpresa)
+      .subscribe((datos) => {
+        this.almacenes = datos;
+      });
+  }
+
+  openDialogWithoutRef() {
+    this.dialog.open(this.dialogNuevoAlmacen, {
+      width: '50%',
+      disableClose: true,
+    });
+  }
+
+  guardar() {
+    this.almacen.codigo = this.form.get('codigo')?.value;
+    this.almacen.almacenNombre = this.form.get('almacenNombre')?.value;
+    this.almacen.central = this.form.get('central')?.value;
+    this.almacen.responsable = this.form.get('responsable')?.value;
+    this.almacen.domicilio = this.form.get('domicilio')?.value;
+    this.almacen.colonia = this.form.get('colonia')?.value;
+    this.almacen.ciudad = this.form.get('ciudad')?.value;
+    this.almacen.telefono = this.form.get('telefono')?.value;
+    this.almacen.idProyecto = this.almacen.central ? null : this.idProyecto;
+    if (
+      typeof this.almacen.codigo === 'undefined' ||
+      !this.almacen.codigo ||
+      this.almacen.codigo === '' ||
+      typeof this.almacen.almacenNombre === 'undefined' ||
+      !this.almacen.almacenNombre ||
+      this.almacen.almacenNombre === '' ||
+      typeof this.almacen.responsable === 'undefined' ||
+      !this.almacen.responsable ||
+      this.almacen.responsable === '' ||
+      typeof this.almacen.domicilio === 'undefined' ||
+      !this.almacen.domicilio ||
+      this.almacen.domicilio === '' ||
+      typeof this.almacen.colonia === 'undefined' ||
+      !this.almacen.colonia ||
+      this.almacen.colonia === '' ||
+      typeof this.almacen.ciudad === 'undefined' ||
+      !this.almacen.ciudad ||
+      this.almacen.ciudad === '' ||
+      typeof this.almacen.telefono === 'undefined' ||
+      !this.almacen.telefono ||
+      this.almacen.telefono === ''
     ) {
-
-
-        this.proyectoSeleccionado = true;
-
-        this.almacenService.obtenerXIdProyecto(this.idProyecto, this.selectedEmpresa).subscribe((datos) => {
-            this.almacenes = datos;
-        });
+      Swal.fire({
+        title: 'Error',
+        text: 'Capture la información correctamente',
+        icon: 'error',
+      });
+      return;
     }
-
-    openDialogWithoutRef() {
-        this.dialog.open(this.dialogNuevoAlmacen, {
-            width: '50%',
-            disableClose: true
-        });
+    if (typeof this.almacen.central === 'undefined' || !this.almacen.central) {
+      this.almacen.central = false;
     }
-
-    guardar() {
-        this.almacen.codigo = this.form.get("codigo")?.value;
-        this.almacen.almacenNombre = this.form.get("almacenNombre")?.value;
-        this.almacen.central = this.form.get("central")?.value;
-        this.almacen.responsable = this.form.get("responsable")?.value;
-        this.almacen.domicilio = this.form.get("domicilio")?.value;
-        this.almacen.colonia = this.form.get("colonia")?.value;
-        this.almacen.ciudad = this.form.get("ciudad")?.value;
-        this.almacen.telefono = this.form.get("telefono")?.value;
-        this.almacen.idProyecto = this.almacen.central? null:  this.idProyecto;
-        if (typeof this.almacen.codigo === 'undefined' || !this.almacen.codigo || this.almacen.codigo === "" ||
-            typeof this.almacen.almacenNombre === 'undefined' || !this.almacen.almacenNombre || this.almacen.almacenNombre === "" ||
-            typeof this.almacen.responsable === 'undefined' || !this.almacen.responsable || this.almacen.responsable === "" ||
-            typeof this.almacen.domicilio === 'undefined' || !this.almacen.domicilio || this.almacen.domicilio === "" ||
-            typeof this.almacen.colonia === 'undefined' || !this.almacen.colonia || this.almacen.colonia === "" ||
-            typeof this.almacen.ciudad === 'undefined' || !this.almacen.ciudad || this.almacen.ciudad === "" ||
-            typeof this.almacen.telefono === 'undefined' || !this.almacen.telefono || this.almacen.telefono === ""
-        ) {
+    console.log('valor de id', this.form.get('id')?.value);
+    if (this.form.get('id')?.value == 0) {
+      this.almacenService
+        .crear(this.almacen, this.selectedEmpresa)
+        .subscribe((datos) => {
+          if (datos.estatus) {
+            this.limpiarFormulario();
+            this.traerInformacion();
+          } else {
             Swal.fire({
-                title: "Error",
-                text: "Capture la información correctamente",
-                icon: "error"
-              });
-            return;
-        }
-        if (typeof this.almacen.central === 'undefined' || !this.almacen.central) {
-            this.almacen.central = false;
-        }
-        console.log("valor de id", this.form.get("id")?.value);
-        if(this.form.get("id")?.value == 0){
-            this.almacenService.crear(this.almacen, this.selectedEmpresa).subscribe((datos) => {
-                if(datos.estatus){
-                    this.limpiarFormulario();
-                    this.traerInformacion();
-                }else{
-                    Swal.fire({
-                        title: "Error",
-                        text: "No se creo el amacén",
-                        icon: "error"
-                      });
-                }
-            })
-        }else{
-            this.nuevoalmacen = this.form.getRawValue();
-            this.almacenService.editar(this.nuevoalmacen, this.selectedEmpresa).subscribe((datos) => {
-                if(datos.estatus){
-                    this.limpiarFormulario();
-                    this.traerInformacion();
-                }else{
-                    Swal.fire({
-                        title: "Error",
-                        text: "No se edito el amacén",
-                        icon: "error"
-                      });
-                }
-            })
-        }
-        this.form.reset();
-        this.form.get("id")?.setValue(0);
-        this.ideditaAlmacen = 0;
+              title: 'Error',
+              text: 'No se creo el amacén',
+              icon: 'error',
+            });
+          }
+        });
+    } else {
+      this.nuevoalmacen = this.form.getRawValue();
+      this.almacenService
+        .editar(this.nuevoalmacen, this.selectedEmpresa)
+        .subscribe((datos) => {
+          if (datos.estatus) {
+            this.limpiarFormulario();
+            this.traerInformacion();
+          } else {
+            Swal.fire({
+              title: 'Error',
+              text: 'No se edito el amacén',
+              icon: 'error',
+            });
+          }
+        });
     }
+    this.form.reset();
+    this.form.get('id')?.setValue(0);
+    this.ideditaAlmacen = 0;
+  }
 
-    limpiarFormulario() {
-        this.form.reset();
-        this.form.get("id")?.setValue(0);
-        this.ideditaAlmacen = 0;
-        this.dialog.closeAll();
-    }
+  limpiarFormulario() {
+    this.form.reset();
+    this.form.get('id')?.setValue(0);
+    this.ideditaAlmacen = 0;
+    this.dialog.closeAll();
+  }
 
-    detenerCierre(event: MouseEvent) {
-        event.stopPropagation();
-    }
+  detenerCierre(event: MouseEvent) {
+    event.stopPropagation();
+  }
 
-    editar(almacen:almacenDTO){
-        this.form.setValue(almacen);
-        this.openDialogWithoutRef();
-    }
+  editar(almacen: almacenDTO) {
+    this.form.setValue(almacen);
+    this.openDialogWithoutRef();
+  }
 
-    cambiarSeleccion(){
-        if(this.mostrarCentrales){
-            this.almacenService.obtenerTodosSinPaginar(this.selectedEmpresa).subscribe({next:(resp)=>{
-                this.almacenes = resp;
-            },error:()=>{
-                //Mensaje de error.
-            }})
-        }else{
-            this.traerInformacion()
-        }
+  get textoMostrarCentrales(): string {
+    return this.mostrarCentrales
+      ? 'Mostrar todos los almacenes'
+      : 'Mostrar centrales';
+  }
+
+  cambiarSeleccion() {
+    if (this.mostrarCentrales) {
+      this.almacenService
+        .obtenerTodosSinPaginar(this.selectedEmpresa)
+        .subscribe({
+          next: (resp) => {
+            this.almacenes = resp;
+          },
+          error: () => {
+            //Mensaje de error.
+          },
+        });
+    } else {
+      this.traerInformacion();
     }
+  }
 }
